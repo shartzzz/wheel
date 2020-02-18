645aeff53979a97f0478bcb07e2cdb0445f6ac3b
shartzzz <funnydist@gmail.com>
2020-02-18 14:09:21 +0300

    Updates

---
+++ precache-manifest.053a68fbc731969ad325b5f23d39c3e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed70f4202f114e9ba3eb6fa7ae7ce58c",
+    "url": "/wheel/index.html"
+  },
+  {
+    "revision": "08b5ce18a0e33163c040",
+    "url": "/wheel/static/css/2.ae2f4c93.chunk.css"
+  },
+  {
+    "revision": "c951769895f8d20178d4",
+    "url": "/wheel/static/css/main.be71dc6b.chunk.css"
+  },
+  {
+    "revision": "08b5ce18a0e33163c040",
+    "url": "/wheel/static/js/2.40122de0.chunk.js"
+  },
+  {
+    "revision": "c951769895f8d20178d4",
+    "url": "/wheel/static/js/main.643b59bd.chunk.js"
+  },
+  {
+    "revision": "d6805298b5727cea3a16",
+    "url": "/wheel/static/js/runtime-main.7ea22eb2.js"
+  },
+  {
+    "revision": "4676cf28f254b9e7c5a4c63dc20d1204",
+    "url": "/wheel/static/media/round.4676cf28.png"
+  }
+]);
